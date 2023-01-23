484f1b393876e90bc0335ab75375197e340d7f29
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-01-23 12:17:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function  submit(){
     let result = document.getElementById("amount");
     let amount = result.value;
-    alert("Ваша заявка в размере " + amount + " рублей успешно отправлена!");
+    let check1 = document.querySelector('.button-switch__checkbox').value;
+    let check2 = document.querySelector('.checkbox-block_check').value;
+    alert("Ваша заявка в размере " + amount + " рублей успешно отправлена!\n"
+          + "С обеспечением: " + check1 + "\n"
+          + "Акция: " + check2 + "\n");
     result.value = "";
 }
 
@@ -9,3 +13,32 @@ document.querySelector('#amountRange').oninput = () => {
     console.log(document.querySelector('#amountRange').value);
     document.querySelector('#amount').value = document.querySelector('#amountRange').value;
 }
+
+document.querySelector('.button-switch__checkbox').addEventListener('click', () => {
+    let with1 = "Да";
+    let withOut = "Нет";
+    if (document.querySelector('.button-switch__checkbox').checked){
+        document.querySelector('.button-switch__checkbox').value = withOut;
+    }
+    else{
+        document.querySelector('.button-switch__checkbox').value = with1;
+    }
+});
+
+
+document.querySelector('.checkbox-block_check').addEventListener('click', () => {
+    let with2 = "Да";
+    let withOut = "Нет";
+    if (document.querySelector('.checkbox-block_check').checked){
+        document.querySelector('.checkbox-block_check').value = with2;
+    }
+    else{
+        document.querySelector('.checkbox-block_check').value = withOut;
+    }
+});
+
+
+document.querySelector('#amountRange').addEventListener('input', () => {
+    let sumNumber = document.querySelector('#amountRange').value;
+    document.querySelector('#amount').value = sumNumber + " ₽";
+});
