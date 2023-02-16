1a9c15c9d61bc6d1786d94ebe2c5a1ec5975c0cc
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-02-16 13:18:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,57 @@
 class test {
-    constructor(){}
+    constructor(){
+
+        document.querySelector('#amountYears').addEventListener('input', () => {
+            let resultYears = document.querySelector('#amountYears').value + ' мес.';
+            console.log(document.querySelector('#amountYears').value);
+            document.querySelector('#amountYear').value = resultYears;
+        });
+
+        document.querySelector('#amountRange').addEventListener('input', () => {
+            let resultSumNumber = document.querySelector('#amountRange').value;
+            console.log(document.querySelector('#amountRange').value);
+            document.querySelector('#amount').value = resultSumNumber;
+        });
+        
+        document.querySelector('.button-switch__checkbox').addEventListener('click', () => {
+            let with1 = "Да";
+            let withOut = "Нет";
+            if (document.querySelector('.button-switch__checkbox').checked){
+                document.querySelector('.button-switch__checkbox').value = withOut;
+            }
+            else{
+                document.querySelector('.button-switch__checkbox').value = with1;
+            }
+        });
+        
+        document.querySelector('.checkbox-block_check').addEventListener('click', () => {
+            let with2 = "Да";
+            let withOut = "Нет";
+            if (document.querySelector('.checkbox-block_check').checked){
+                document.querySelector('.checkbox-block_check').value = with2;
+            }
+            else{
+                document.querySelector('.checkbox-block_check').value = withOut;
+            }
+        });
+        
+        document.querySelector('#amountRange').addEventListener('input', () => {
+            let sumNumberRange = document.querySelector('#amountRange').value;
+            let showSumNumber = new Intl.NumberFormat('ru-RU', {
+                style: "currency",
+                currency: 'RUB'
+            }).format(sumNumberRange);
+        
+            document.querySelector('#amount').value = showSumNumber;
+        });
+
+        let Sum = document.querySelector('#amountRange').value;
+        let Years = document.querySelector('#amountYears').value;
+        let percent = 16.5 / 12 / 100;
+        let isn = Math.pow(1 + percent, Years);
+        let rr = Math.ceil(Sum * percent * (isn/(isn - 1))) + ' ₽/мес.';
+        document.querySelector('.content-title').textContent = rr;
+    }   
 
     submit(){
         document.querySelector(".button-to-send").addEventListener('click', () => {
@@ -31,45 +83,7 @@ class test {
     }
 }
 
+
 let test1 = new test();
 test1.getResponse();
 test1.submit();
-
-
-
-document.querySelector('#amountRange').oninput = () => {
-    console.log(document.querySelector('#amountRange').value);
-    document.querySelector('#amount').value = document.querySelector('#amountRange').value;
-}
-
-document.querySelector('.button-switch__checkbox').addEventListener('click', () => {
-    let with1 = "Да";
-    let withOut = "Нет";
-    if (document.querySelector('.button-switch__checkbox').checked){
-        document.querySelector('.button-switch__checkbox').value = withOut;
-    }
-    else{
-        document.querySelector('.button-switch__checkbox').value = with1;
-    }
-});
-
-document.querySelector('.checkbox-block_check').addEventListener('click', () => {
-    let with2 = "Да";
-    let withOut = "Нет";
-    if (document.querySelector('.checkbox-block_check').checked){
-        document.querySelector('.checkbox-block_check').value = with2;
-    }
-    else{
-        document.querySelector('.checkbox-block_check').value = withOut;
-    }
-});
-
-document.querySelector('#amountRange').addEventListener('input', () => {
-    let sumNumberRange = document.querySelector('#amountRange').value;
-    let showSumNumber = new Intl.NumberFormat('ru-RU', {
-        style: "currency",
-        currency: 'RUB'
-    }).format(sumNumberRange);
-
-    document.querySelector('#amount').value = showSumNumber;
-});
