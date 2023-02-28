f7aa51a3caa39f65a6c82d2bdaa52c24d8efc7fc
SnowPenguin01 <122963898+SnowPenguin01@users.noreply.github.com>
2023-02-28 10:30:23 +0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,17 @@
 class test {
     constructor(){
 
-        document.querySelector('#amountRange').oninput = () => {
+        document.querySelector('#amountYears').addEventListener('input', () => {
+            let resultYears = document.querySelector('#amountYears').value + ' мес.';
+            console.log(document.querySelector('#amountYears').value);
+            document.querySelector('#amountYear').value = resultYears;
+        });
+
+        document.querySelector('#amountRange').addEventListener('input', () => {
+            let resultSumNumber = document.querySelector('#amountRange').value;
             console.log(document.querySelector('#amountRange').value);
-            document.querySelector('#amount').value = document.querySelector('#amountRange').value;
-        }
+            document.querySelector('#amount').value = resultSumNumber;
+        });
         
         document.querySelector('.button-switch__checkbox').addEventListener('click', () => {
             let with1 = "Да";
@@ -37,38 +44,69 @@ class test {
         
             document.querySelector('#amount').value = showSumNumber;
         });
+
+
+        document.querySelector('#amountRange').addEventListener('input', sumCred => {
+            document.querySelector('#amountYears').addEventListener('input', yearNum =>{
+                let Sum = sumCred;
+                console.log(Sum);
+                let Years = yearNum;
+                console.log(Years);
+                let percent = 16.5 / 12 / 100;
+                let isn = Math.pow(1 + percent, Years.target.value);
+                let rr = Math.ceil(Sum.target.value * percent * (isn/(isn - 1))) + ' ₽/мес.';
+                document.querySelector('.content-title').textContent = rr;
+            });
+        })
     }
 
     submit(){
         document.querySelector(".button-to-send").addEventListener('click', () => {
-            let result = document.getElementById("amount");
-            let amount = result.value;
+            let amount = document.getElementById("amount");
+            let resetAmount = amount.value;
+            let data = document.getElementById("amountYear");
+            let resetData = data.value;
             let check1 = document.querySelector('.button-switch__checkbox').value;
             let check2 = document.querySelector('.checkbox-block_check').value;
-            alert("Ваша заявка в размере " + amount + " рублей успешно отправлена!\n"
+            alert("Ваша заявка в размере " + resetAmount + " рублей успешно отправлена!\n"
                   + "С обеспечением: " + check1 + "\n"
-                  + "Акция: " + check2 + "\n");
-            result.value = "";
+                  + "Акция: " + check2 + "\n"
+                  + "На срок кредита: " + resetData + "\n");
+            amount.value = "";
+            data.value = "";
         });
     }
 
-    async getResponse(){
-        let response = await fetch('http://new.energobank.su/local/ajax/calculator.php?PID=15373');
-        let result = await response.json();
-        
-        //console.log('result', result);
-        let creditNumberMin = result.sum.min_value + ' ₽';
-        let creditNumberMax = result.sum.max_value + ' ₽';
-        let creditDataMin = result.term.min_value + ' мес.';
-        let creditDataMax = (result.term.max_value / 12) + ' лет';
-        
-        document.querySelector(".credit-number__min").innerHTML = creditNumberMin;
-        document.querySelector(".credit-number__max").innerHTML = creditNumberMax;
-        document.querySelector(".credit-data__min").innerHTML = creditDataMin;
-        document.querySelector(".credit-data__max").innerHTML = creditDataMax;
+    getResponse(){
+
+        fetch('http://new.energobank.su/local/ajax/calculator.php?PID=15373')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data),
+                document.querySelector(".credit-number__min").innerHTML = data.sum.min_value + ' ₽',
+                document.querySelector(".credit-number__max").innerHTML = data.sum.max_value + ' ₽',
+                document.querySelector(".credit-data__min").innerHTML = data.term.min_value + ' мес.',
+                document.querySelector(".credit-data__max").innerHTML = (data.term.max_value / 12) + ' лет'
+            })
     }
+
+    // async getResponse(){
+    //     let response = await fetch('http://new.energobank.su/local/ajax/calculator.php?PID=15373');
+    //     let result = await response.json();
+        
+    //     //console.log('result', result);
+    //     let creditNumberMin = result.sum.min_value + ' ₽';
+    //     let creditNumberMax = result.sum.max_value + ' ₽';
+    //     let creditDataMin = result.term.min_value + ' мес.';
+    //     let creditDataMax = (result.term.max_value / 12) + ' лет';
+        
+    //     document.querySelector(".credit-number__min").innerHTML = creditNumberMin;
+    //     document.querySelector(".credit-number__max").innerHTML = creditNumberMax;
+    //     document.querySelector(".credit-data__min").innerHTML = creditDataMin;
+    //     document.querySelector(".credit-data__max").innerHTML = creditDataMax;
+    // }
 }
 
-let showResultTest = new test();
-showResultTest.getResponse();
-showResultTest.submit();
+let test1 = new test();
+test1.getResponse();
+test1.submit();
